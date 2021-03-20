a0d91141bf72e75dfa37ca07e1094ff459013984
maxpro99 <78781637+maxpro99@users.noreply.github.com>
2021-03-20 19:58:55 +0800

    only task_7_2.py

--- task_7_2.py
+++ task_7_2.py
@@ -8,15 +8,40 @@
 # - Совершенно непонятно как файлы от папок отличать
 # - Надо бы обрабатывать комментарии
 
+# Не ясно почему yaml.
+# Не разбирали его, ни ссылок не видел на него...
+# Да и в самом стандарте
+# https://yaml.org/spec/1.2/spec.html
+# описание приведенного примера не нашел
+# Например "|--" - что за покемон?
+# Непонятно также почему на первом уровне
+# продолжается часть этого знака во всех строках
+# а на нулевом нет? и на втором и на третьем нет?
+# Непонятно как файлы от папок отличать
+
 import os
 
-# Предусмотрены вложения не дальше 9 уровня!!!
 with open('config.yaml', 'r', encoding='utf-8') as f:
-   data = (line for line in f)
-   for line in f:
-      path[line.count('   ')] = line.strip().replace('|--','').replace(' ','')
-      print(path[:line.count('   ')+1])
+    data = f.readlines()
 
-path[0] = '1'
-print(path)
-     # (mk_obj(_) for _ in f.readlines(f))
+path = []
+last_level = -1
+for line in data:
+    level = line.count('   ') + line.count('|  ')
+    if level > last_level:
+        path.append(line.strip().replace('|--', '').replace('|  ', '').replace('   ', ''))
+    else:
+        path[level] = line.strip().replace('|--', '').replace('|  ', '').replace('   ', '')
+        for i in range(level, last_level):
+            path.pop()
+
+    # Папки от файфлов в данном примере будем различать наличием точки
+    # И это опять плохая практика
+    if '.' in path[level]:
+        with open(os.path.join(*path), 'x', encoding='utf-8') as f:
+            f.write('я файлик')
+    else:
+        if not os.path.exists(os.path.join(*path)):
+            os.mkdir(os.path.join(*path))
+    last_level = level
+
